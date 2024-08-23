404b39ecf08ca4c7a6395363fd05dd04a3295760
Kishore Veleti <vavkkishore@Kishores-Air.lan>
2024-08-23 14:35:27 -0400

    Initial commit

---
+++ src/main/java/com/jk/research/trading_app/TradingBookCrudAppApplication.java
@@ -0,0 +1,13 @@
+package com.jk.research.trading_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradingBookCrudAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradingBookCrudAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jk/research/trading_app/TradingBookCrudAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jk.research.trading_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradingBookCrudAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
